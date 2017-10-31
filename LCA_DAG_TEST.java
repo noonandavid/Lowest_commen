1c67104083d7bf60000744485ea3fcf9e3a5ed2b
noonandavid <32358791+noonandavid@users.noreply.github.com>
2017-10-31 17:47:29 +0000

    Add files via upload

--- LCA_DAG_TEST.java
+++ LCA_DAG_TEST.java
@@ -9,7 +9,7 @@ public class LCA_DAG_TEST {
 	@Test
 	public void test() {
 		DAG_LCA<Integer, Integer> dag  = new DAG_LCA<Integer, Integer>();
-		assertEquals(-1,dag.lowestCommenAncestor(3,4));
+		assertEquals("Try LCA on an empty tree",-1,dag.lowestCommenAncestor(3,4));
 		dag.create(1);
 		dag.create(2);
 		dag.create(3);
@@ -19,24 +19,24 @@ public class LCA_DAG_TEST {
 		dag.create(8);
 		dag.create(9);
 		dag.create(7);
-		assertEquals(true,dag.addParent(1, 0));
-		assertEquals(true,dag.addParent(2, 0));
-		assertEquals(true,dag.addParent(3, 2));
-		assertEquals(true,dag.addParent(4, 2));
-		assertEquals(true,dag.addParent(5, 4));
-		assertEquals(true,dag.addParent(5, 3));
-		assertEquals(true,dag.addParent(6, 1));
-		assertEquals(true,dag.addParent(7, 6));
-		assertEquals(true,dag.addParent(7, 4));
-		assertEquals(true,dag.addParent(9, 4));
-		assertEquals(true,dag.addParent(8, 5));
-		assertEquals(false,dag.addParent(2, 8));
-		assertEquals(2,dag.lowestCommenAncestor(3,4));
-		assertEquals(0,dag.lowestCommenAncestor(5,6));
-		assertEquals(4,dag.lowestCommenAncestor(8,7));
-		assertEquals(2,dag.lowestCommenAncestor(3,9));
-		assertEquals(-1,dag.lowestCommenAncestor(3,956));
-		assertEquals(-1,dag.lowestCommenAncestor(333,933));
+		assertEquals("Add a parent",true,dag.addParent(1, 0));
+		assertEquals("Add a parent",true,dag.addParent(2, 0));
+		assertEquals("Add a parent",true,dag.addParent(3, 2));
+		assertEquals("Add a parent",true,dag.addParent(4, 2));
+		assertEquals("Add a parent",true,dag.addParent(5, 4));
+		assertEquals("Add a parent",true,dag.addParent(5, 3));
+		assertEquals("Add a parent",true,dag.addParent(6, 1));
+		assertEquals("Add a parent",true,dag.addParent(7, 6));
+		assertEquals("Add a parent",true,dag.addParent(7, 4));
+		assertEquals("Add a parent",true,dag.addParent(9, 4));
+		assertEquals("Add a parent",true,dag.addParent(8, 5));
+		assertEquals("Add a parent that creates a loop",false,dag.addParent(2, 8));
+		assertEquals("Test a normal lca",2,dag.lowestCommenAncestor(3,4));
+		assertEquals("Test a normal lca",0,dag.lowestCommenAncestor(5,6));
+		assertEquals("Test a normal lca",4,dag.lowestCommenAncestor(8,7));
+		assertEquals("Test a normal lca",2,dag.lowestCommenAncestor(3,9));
+		assertEquals("Test an lca with one node missing",-1,dag.lowestCommenAncestor(3,956));
+		assertEquals("Test an lca with both nodes missing",-1,dag.lowestCommenAncestor(333,933));
 		
 		
 	}
